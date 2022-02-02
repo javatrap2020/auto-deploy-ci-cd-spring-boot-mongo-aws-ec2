8eb0cc239af64527febbf2d724a6785a09bc764e
Oleh Boiko <javatrap2020@gmail.com>
2022-02-02 14:46:02 +0200

    model and utils

--- src/main/java/com/javatrap2020/patient/model/Patient.java
+++ src/main/java/com/javatrap2020/patient/model/Patient.java
@@ -1,4 +1,21 @@
 package com.javatrap2020.patient.model;
 
-public class Patient {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collation = "patient")
+public class Patient implements Serializable {
+    @Id
+    private String id;
+    private String name;
 }
--- src/main/java/com/javatrap2020/patient/utils/HelperUtil.java
+++ src/main/java/com/javatrap2020/patient/utils/HelperUtil.java
@@ -1,4 +1,21 @@
 package com.javatrap2020.patient.utils;
 
+import com.javatrap2020.patient.model.Patient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class HelperUtil {
+
+    private HelperUtil() {
+
+    }
+
+    public static Supplier<List<Patient>> patientDataSupplier = () ->
+            Arrays.asList(
+                    Patient.builder().name("Patient1").build(),
+                    Patient.builder().name("Patient2").build(),
+                    Patient.builder().name("Patient3").build()
+            );
 }
