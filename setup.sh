39ada8636ceaa6dd903fe8aa4e2ef6f0b9f67f4d
Oleh Boiko <javatrap2020@gmail.com>
2022-02-05 15:15:51 +0200

    fix6_14

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo chmod 666 /var/run/docker.sock
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+#sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
