a87e92bdd8cb21f966f4bc30e37da5d9b7d78b2c
Oleh Boiko <javatrap2020@gmail.com>
2022-02-07 23:24:34 +0200

    setting appspec-021

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-sudo amazon-linux-extras install docker
+sudo amazon-linux-extras install docker -y
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo service docker start
-sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo dockerd --debug
 sudo usermod -aG docker ec2-user
-sudo service docker start
+sudo chmod 666 /var/run/docker.sock
+sudo docker ps -a
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo su
 yum update -y
 aws s3 cp s3://deploy-git-bucket-user3/.env /home/ec2-user/server
-cd server/
+exit
+cd /home/ec2-user/server
 sudo docker-compose up
