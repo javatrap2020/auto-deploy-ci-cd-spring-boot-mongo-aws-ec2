480747c56d1334ba3a7a0b8dc5aa5ce12aa07879
Oleh Boiko <javatrap2020@gmail.com>
2022-02-07 14:55:37 +0200

    setting appspec-001

--- src/main/java/com/javatrap2020/patient/controller/PatientController.java
+++ src/main/java/com/javatrap2020/patient/controller/PatientController.java
@@ -4,15 +4,11 @@ import com.javatrap2020.patient.model.Patient;
 import com.javatrap2020.patient.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.UnknownHostException;
