60f1e2e5f45c91c4df2bec5ffdae610370c8e303
Oleh Boiko <javatrap2020@gmail.com>
2022-02-07 22:12:09 +0200

    setting appspec-018

--- scripts/kill_container.sh
+++ scripts/kill_container.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 cd /home/ec2-user/server
-sudo docker-compose down
+sudo docker ps -a
+sudo docker stop spring-boot-docker
+sudo docker rm spring-boot-docker
+sudo docker images
+sudo docker image rmi -f javatrap/spring-boot-docker
