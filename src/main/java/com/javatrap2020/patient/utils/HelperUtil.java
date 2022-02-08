9e17af4c57d8c084a8f092251aa800283ee9148c
Oleh Boiko <javatrap2020@gmail.com>
2022-02-08 09:32:56 +0200

    test7

--- src/main/java/com/javatrap2020/patient/utils/HelperUtil.java
+++ src/main/java/com/javatrap2020/patient/utils/HelperUtil.java
@@ -14,10 +14,9 @@ public class HelperUtil {
 
     public static Supplier<List<Patient>> patientDataSupplier = () ->
             Arrays.asList(
-                    Patient.builder().name("Patient1-1").email("javatrap@java.com").build(),
+                    Patient.builder().name("Patient1").email("javatrap@java.com").build(),
                     Patient.builder().name("Patient2").email("javatrap2@java.com").build(),
-                    Patient.builder().name("Patient3").email("javatrap3@java.com").build(),
-                    Patient.builder().name("Patient4").email("javatrap4@java.com").build(),
-                    Patient.builder().name("Patient5").email("javatrap5@java.com").build()
+                    Patient.builder().name("Patient3").email("javatrap3@java.com").build()
+
             );
 }
